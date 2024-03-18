e649138d9fb445d97465da50b02aa71da9578b5a
muhammadhaseebthair <haseebthair7860@gmail.com>
2024-03-18 21:34:50 +0500

    work complete

---
+++ app/app.js
@@ -0,0 +1,65 @@
+// **********Alert Name****
+function alertName(){
+    alert("Muhammad Haseeb Tahir")
+}
+// **********Alert Number****
+
+function alertNumber(){
+    alert("+92 346551036")
+}
+// **********Alert Number****
+function variableName(){
+    alert("var a=10 \n var b=20")
+}
+// *********Camel Case*********
+
+function camelCase(){
+    alert(" Camel Case Example 'firstName'")
+}
+// *******Sum two number*******
+function sum2Number(){
+var a= 10;
+var b= 20;
+var c= a+b;
+alert(  "a=10 & b=20 \n 10+ 20=" +c )
+
+}
+
+// *******Subtract two number*******
+
+function subtract2Number(){
+    var a= 20;
+    var b= 20;
+    var c= a-b;
+    alert(  "a=20 & b=20 \n 20-20=" +c )
+     
+}
+// *******Multiply two number*******
+function multiplyNumber(){
+    var a= 20;
+    var b= 20;
+    var c= a*b;
+    alert(  "a=20 & b=20 \n 20*20=" +c )
+    
+}
+// *******Divide two number*******
+function divideNumber(){
+    var a= 20;
+    var b= 5;
+    var c= a/b;
+    alert(  "a=20 & b=5 \n 20/5=" +c )
+    
+}
+// *******Clear statement******
+
+function clearstatement(){
+var clear= document.getElementById("clearStatementBtn")
+clear.value = "";
+}
+
+// *******clearoutput**********
+
+function clears(){
+   var output= document.getElementById("clearoutput");
+   output.value=" ";
+}
